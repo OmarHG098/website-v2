68f085f341f64c3cea6541a40b61a3352f1c694d
OmarHG098 <omar_ehr@outlook.com>
2025-08-04 16:15:20 +0000

    UPDATE: fs full time prices

--- src/components/PricesAndPayment/index.js
+++ src/components/PricesAndPayment/index.js
@@ -809,7 +809,7 @@ const PricesAndPayment = (props) => {
               gridGap="32px 0px"
               className="inner-container"
             >
-              {availablePlans.some((plan) => plan.price) && (
+             {/* {availablePlans.some((plan) => plan.price) && (
                 <Div
                   className="job-guarantee"
                   padding="8px"
@@ -837,7 +837,7 @@ const PricesAndPayment = (props) => {
                     {info.job_guarantee.description}
                   </Paragraph>
                 </Div>
-              )}
+              )}*/}
               {availablePlans && availablePlans.length > 0 && (
                 <Div
                   className="bullets-container"
