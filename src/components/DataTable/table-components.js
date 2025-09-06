c3cfb84b354c03dbc1d965ad239ad8e713b4a226
ArielCalisaya <51676628+ArielCalisaya@users.noreply.github.com>
2025-09-06 19:45:13 -0400

    style(DataTable): standardize string quotes and improve code formatting

--- src/components/DataTable/table-components.js
+++ src/components/DataTable/table-components.js
@@ -61,7 +61,9 @@ const CellContent = React.memo(({ cell, cellStyle, isHeaderCell = false }) => {
           icon={cell.icon}
           width={cell.size || "16px"}
           height={cell.size || "16px"}
-          color={cell.icon_color || (isHeaderCell ? Colors.darkGray : Colors.black)}
+          color={
+            cell.icon_color || (isHeaderCell ? Colors.darkGray : Colors.black)
+          }
         />
       )}
       {hasContent && (
@@ -81,7 +83,9 @@ const CellContent = React.memo(({ cell, cellStyle, isHeaderCell = false }) => {
           style={{ flexShrink: 0, ...cell.icon_style }}
           width={cell.size || "16px"}
           height={cell.size || "16px"}
-          color={cell.icon_color || (isHeaderCell ? Colors.darkGray : Colors.black)}
+          color={
+            cell.icon_color || (isHeaderCell ? Colors.darkGray : Colors.black)
+          }
         />
       )}
     </Div>
@@ -144,168 +148,189 @@ const ActionButtons = React.memo(({ actions }) => (
   </Div>
 ));
 
-const TableHeader = React.memo(({
-  columns,
-  headerStyle,
-  stickyHeaders,
-  gridColumns,
-  gridColumns_tablet,
-  childBorderRadius,
-  borderRadiusValue,
-}) => {
-  const stickyHeaderStyles = stickyHeaders ? {
-    position: "sticky",
-    top: "0",
-    zIndex: "10",
-    backgroundColor: Colors.white,
-    boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-  } : {};
+const TableHeader = React.memo(
+  ({
+    columns,
+    headerStyle,
+    stickyHeaders,
+    gridColumns,
+    gridColumns_tablet,
+    childBorderRadius,
+    borderRadiusValue,
+  }) => {
+    const stickyHeaderStyles = stickyHeaders
+      ? {
+          position: "sticky",
+          top: "0",
+          zIndex: "10",
+          backgroundColor: Colors.white,
+          boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+        }
+      : {};
 
-  const [topLeft, topRight] = childBorderRadius.split(" ");
+    const [topLeft, topRight] = childBorderRadius.split(" ");
 
-  return (
-    <Div
-      as="thead"
-      display="grid"
-      gridTemplateColumns={gridColumns}
-      gridTemplateColumns_tablet={gridColumns_tablet}
-      gridGap="1px"
-      {...stickyHeaderStyles}
-    >
-      <Div as="tr" display="contents">
-        {columns.map((column, index) => {
-          const isFirst = index === 0;
-          const isLast = index === columns.length - 1;
-          
-          let headerBorderRadius = "0px";
-          if (isFirst) headerBorderRadius = `${topLeft} 0px 0px 0px`;
-          else if (isLast) headerBorderRadius = `0px ${topRight} 0px 0px`;
+    return (
+      <Div
+        as="thead"
+        display="grid"
+        gridTemplateColumns={gridColumns}
+        gridTemplateColumns_tablet={gridColumns_tablet}
+        gridGap="1px"
+        {...stickyHeaderStyles}
+      >
+        <Div as="tr" display="contents">
+          {columns.map((column, index) => {
+            const isFirst = index === 0;
+            const isLast = index === columns.length - 1;
 
-          return (
-            <Div
-              as="th"
-              key={`header-${index}`}
-              background={isFirst ? Colors.black : Colors.white}
-              padding={stickyHeaders ? "8px 16px" : "15px"}
-              padding_tablet="20px"
-              display="flex"
-              alignItems="center"
-              justifyContent={isFirst ? "flex-start" : "center"}
-              borderBottom={`2px solid ${Colors.lightGray}`}
-              borderRight={index < columns.length - 1 ? `1px solid ${Colors.borderGray}` : "none"}
-              borderRadius={headerBorderRadius}
-              minWidth="120px"
-              minWidth_tablet="auto"
-              whiteSpace="nowrap"
-              whiteSpace_tablet="normal"
-              scope={isFirst ? "row" : "col"}
-              {...headerStyle}
-            >
-              <H3
-                type="h3"
-                fontSize={stickyHeaders ? "14px" : "16px"}
-                fontSize_tablet="18px"
-                fontWeight="600"
-                color={isFirst ? Colors.white : Colors.darkGray}
-                margin="0"
-                textAlign={isFirst ? "left" : "center"}
+            let headerBorderRadius = "0px";
+            if (isFirst) headerBorderRadius = `${topLeft} 0px 0px 0px`;
+            else if (isLast) headerBorderRadius = `0px ${topRight} 0px 0px`;
+
+            return (
+              <Div
+                as="th"
+                key={`header-${index}`}
+                background={isFirst ? Colors.black : Colors.white}
+                padding={stickyHeaders ? "8px 16px" : "15px"}
+                padding_tablet="20px"
+                display="flex"
+                alignItems="center"
+                justifyContent={isFirst ? "flex-start" : "center"}
+                borderBottom={`2px solid ${Colors.lightGray}`}
+                borderRight={
+                  index < columns.length - 1
+                    ? `1px solid ${Colors.borderGray}`
+                    : "none"
+                }
+                borderRadius={headerBorderRadius}
+                minWidth="120px"
+                minWidth_tablet="auto"
+                whiteSpace="nowrap"
+                whiteSpace_tablet="normal"
+                scope={isFirst ? "row" : "col"}
+                {...headerStyle}
               >
-                {column.title || column}
-              </H3>
-            </Div>
-          );
-        })}
+                <H3
+                  type="h3"
+                  fontSize={stickyHeaders ? "14px" : "16px"}
+                  fontSize_tablet="18px"
+                  fontWeight="600"
+                  color={isFirst ? Colors.white : Colors.darkGray}
+                  margin="0"
+                  textAlign={isFirst ? "left" : "center"}
+                >
+                  {column.title || column}
+                </H3>
+              </Div>
+            );
+          })}
+        </Div>
       </Div>
-    </Div>
-  );
-});
-
-const TableCell = React.memo(({
-  cell,
-  cellIndex,
-  rowIndex,
-  totalRows,
-  totalColumns,
-  cellStyle,
-  childBorderRadius,
-}) => {
-  const isFirstColumn = cellIndex === 0;
-  const isLastRow = rowIndex === totalRows - 1;
-  const isLastColumn = cellIndex === totalColumns - 1;
-
-  const backgroundColor = (!cellStyle?.disableLinePattern && rowIndex % 2 === 1) 
-    ? (cellStyle?.linePatternColor || Colors.veryLightBlue3) 
-    : Colors.white;
-
-  let cellBorderRadius = "0px";
-  if (isLastRow) {
-    const borderValues = childBorderRadius.split(" ");
-    const bottomRight = borderValues[2] || borderValues[0];
-    const bottomLeft = borderValues[3] || borderValues[2] || borderValues[0];
-    
-    if (isFirstColumn) cellBorderRadius = `0px 0px 0px ${bottomLeft}`;
-    else if (isLastColumn) cellBorderRadius = `0px 0px ${bottomRight} 0px`;
+    );
   }
+);
 
-  return (
-    <Div
-      as={isFirstColumn ? "th" : "td"}
-      key={`cell-${rowIndex}-${cellIndex}`}
-      background={backgroundColor}
-      padding="15px"
-      padding_tablet="20px"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      borderBottom={rowIndex < totalRows - 1 ? `1px solid ${Colors.borderGray}` : "none"}
-      borderRight={cellIndex < totalColumns - 1 ? `1px solid ${Colors.borderGray}` : "none"}
-      borderRadius={cellBorderRadius}
-      minHeight="80px"
-      minWidth="120px"
-      minWidth_tablet="auto"
-      whiteSpace="nowrap"
-      whiteSpace_tablet="normal"
-      scope={isFirstColumn ? "row" : undefined}
-      {...cellStyle}
-    >
-      <CellContent
-        cell={cell}
-        cellStyle={cellStyle}
-        isHeaderCell={isFirstColumn}
-      />
-      {(cell.primary_action || cell.secondary_action) && (
-        <ActionButtons actions={{
-          primary: cell.primary_action,
-          secondary: cell.secondary_action
-        }} />
-      )}
-    </Div>
-  );
-});
+const TableCell = React.memo(
+  ({
+    cell,
+    cellIndex,
+    rowIndex,
+    totalRows,
+    totalColumns,
+    cellStyle,
+    childBorderRadius,
+  }) => {
+    const isFirstColumn = cellIndex === 0;
+    const isLastRow = rowIndex === totalRows - 1;
+    const isLastColumn = cellIndex === totalColumns - 1;
 
-const TableRow = React.memo(({
-  row,
-  rowIndex,
-  totalRows,
-  totalColumns,
-  cellStyle,
-  childBorderRadius,
-}) => (
-  <Div as="tr" key={`row-${rowIndex}`} display="contents">
-    {row.cells?.map((cell, cellIndex) => (
-      <TableCell
+    const backgroundColor =
+      !cellStyle?.disableLinePattern && rowIndex % 2 === 1
+        ? cellStyle?.linePatternColor || Colors.veryLightBlue3
+        : Colors.white;
+
+    let cellBorderRadius = "0px";
+    if (isLastRow) {
+      const borderValues = childBorderRadius.split(" ");
+      const bottomRight = borderValues[2] || borderValues[0];
+      const bottomLeft = borderValues[3] || borderValues[2] || borderValues[0];
+
+      if (isFirstColumn) cellBorderRadius = `0px 0px 0px ${bottomLeft}`;
+      else if (isLastColumn) cellBorderRadius = `0px 0px ${bottomRight} 0px`;
+    }
+
+    return (
+      <Div
+        as={isFirstColumn ? "th" : "td"}
         key={`cell-${rowIndex}-${cellIndex}`}
-        cell={cell}
-        cellIndex={cellIndex}
-        rowIndex={rowIndex}
-        totalRows={totalRows}
-        totalColumns={totalColumns}
-        cellStyle={cellStyle}
-        childBorderRadius={childBorderRadius}
-      />
-    ))}
-  </Div>
-));
+        background={backgroundColor}
+        padding="15px"
+        padding_tablet="20px"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        borderBottom={
+          rowIndex < totalRows - 1 ? `1px solid ${Colors.borderGray}` : "none"
+        }
+        borderRight={
+          cellIndex < totalColumns - 1
+            ? `1px solid ${Colors.borderGray}`
+            : "none"
+        }
+        borderRadius={cellBorderRadius}
+        minHeight="80px"
+        minWidth="120px"
+        minWidth_tablet="auto"
+        whiteSpace="nowrap"
+        whiteSpace_tablet="normal"
+        scope={isFirstColumn ? "row" : undefined}
+        {...cellStyle}
+      >
+        <CellContent
+          cell={cell}
+          cellStyle={cellStyle}
+          isHeaderCell={isFirstColumn}
+        />
+        {(cell.primary_action || cell.secondary_action) && (
+          <ActionButtons
+            actions={{
+              primary: cell.primary_action,
+              secondary: cell.secondary_action,
+            }}
+          />
+        )}
+      </Div>
+    );
+  }
+);
+
+const TableRow = React.memo(
+  ({
+    row,
+    rowIndex,
+    totalRows,
+    totalColumns,
+    cellStyle,
+    childBorderRadius,
+  }) => (
+    <Div as="tr" key={`row-${rowIndex}`} display="contents">
+      {row.cells?.map((cell, cellIndex) => (
+        <TableCell
+          key={`cell-${rowIndex}-${cellIndex}`}
+          cell={cell}
+          cellIndex={cellIndex}
+          rowIndex={rowIndex}
+          totalRows={totalRows}
+          totalColumns={totalColumns}
+          cellStyle={cellStyle}
+          childBorderRadius={childBorderRadius}
+        />
+      ))}
+    </Div>
+  )
+);
 
 export { CellContent, ActionButtons, TableHeader, TableCell, TableRow };
