2c54d14c7271b181531e87ae9bb513c4aaad6a83
OmarHG098 <omar_ehr@outlook.com>
2025-09-16 16:28:55 +0000

    UPDATE: header section

--- src/templates/geekpal.js
+++ src/templates/geekpal.js
@@ -7,269 +7,218 @@ import { VerticalVideoHolder } from "../components/Styling";
 import ReactPlayer from "../components/ReactPlayer";
 import { Div, Grid } from "../components/Sections";
 import { Paragraph } from "../components/Heading";
-import { Img } from "../components/Styling";
-import { StyledBackgroundSection } from "../components/Styling";
+import { Img, StyledBackgroundSection, Colors } from "../components/Styling";
+import Icon from "../components/Icon";
+import { isCustomBarActive } from "../actions";
 
-// Componente para elementos decorativos reutilizables
-const DecorativeElements = ({ variant = "header", position = "right" }) => {
-  const decorativeConfigs = {
-    header: {
-      ellipse: {
-        src: "/images/Ellipse-7.png",
-        width: "450px",
-        height: "300px",
-        style: {
-          position: "absolute",
-          right: "-150px",
-          top: "-100px",
-        },
-        display_xs: "none",
-      },
-      vectorStroke: {
-        src: "/images/vector-stroke-light.png",
-        width: "120px",
-        height: "165px",
-        style: {
-          position: "absolute",
-          left: "48%",
-          top: "20%",
-        },
-        display_xs: "none",
-      },
-    },
-    section: {
-      vectorBottom: {
-        src: "/images/vector-stroke-left.png",
-        width: "89px",
-        height: "119px",
-        style: {
-          position: "absolute",
-          left: "-62px",
-          bottom: "21px",
-          zIndex: "-1",
-        },
-      },
-    },
-  };
-
-  const config = decorativeConfigs[variant];
-  if (!config) return null;
-
-  return (
-    <>
-      {Object.entries(config).map(([key, props]) => (
-        <Img key={key} backgroundSize="contain" {...props} />
-      ))}
-    </>
-  );
-};
-
-// Componente del header principal con elementos gráficos
-const GeekPalHeader = ({ yml, session }) => {
-  return (
-    <Grid
-      padding="24px 20px"
-      padding_tablet="100px 40px 40px 40px"
-      padding_md="100px 80px 40px 80px"
-      padding_lg="100px 0px 40px 0px"
-      columns_tablet="18"
-      margin="70px auto 24px auto"
-      maxWidth="1280px"
-      position="relative"
-      gridTemplateColumns_tablet="repeat(21, 1fr)"
-      gridGap="0px"
-    >
-      {/* Elementos decorativos del header */}
-      <DecorativeElements variant="header" />
-
-      {/* Contenido del header */}
-      <Div
-        flexDirection="column"
-        justifyContent_tablet="start"
-        gridColumn_tablet="1 / 11"
-        position="relative"
-      >
-        <Paragraph
-          fontSize="50px"
-          lineHeight="54px"
-          padding_xs="10px 0"
-          padding_md="0px"
-          margin="0"
-          textAlign_xs="center"
-          textAlign_tablet="left"
-          color="black"
-          fontSize_xs="40px"
-          lineHeight_xs="38px"
-          fontSize_tablet="50px"
-          lineHeight_tablet="54px"
-          dangerouslySetInnerHTML={{ __html: yml.header.title }}
-        />
-        <Paragraph
-          fontSize="24px"
-          lineHeight="29px"
-          fontFamily="Lato"
-          padding_xs="10px 0px"
-          textAlign_xs="center"
-          textAlign_tablet="left"
-          padding_md="30px 0px 10px 0"
-          color="black"
-          fontSize_xs="18px"
-          lineHeight_xs="24px"
-          fontSize_tablet="24px"
-          lineHeight_tablet="29px"
-          dangerouslySetInnerHTML={{ __html: yml.header.paragraph }}
-        />
-
-        {/* Decoración vectorial para la sección */}
-        <DecorativeElements variant="section" />
-      </Div>
-
-      {/* Contenedor de video/imagen con decoraciones */}
-      <VideoImageContainer yml={yml} />
-    </Grid>
-  );
-};
-
-// Componente separado para el contenedor de video/imagen
-const VideoImageContainer = ({ yml }) => {
-  return (
-    <Div
-      height="auto"
-      width="80%"
-      width_tablet="80%"
-      width_md="75%"
-      width_xs="100%"
-      padding_xs="40px 0 0 0"
-      padding_tablet="15% 0 0 0"
-      padding_md="0% 16px 0 5%"
-      padding_lg="0 15px 0 5%"
-      gridColumn_tablet="11 / 22"
-      position="relative"
-      margin="0 auto"
-    >
-      {/* Decoración inferior */}
-      <Img
-        src="/images/Group-6400.png"
-        width="193px"
-        height="10px"
-        style={{ position: "absolute" }}
-        left_xs="0"
-        bottom_tablet="10%"
-        left_md="10%"
-        bottom_md="10%"
-        bottom_lg="0%"
-        display_xs="none"
-      />
-
-      <VerticalVideoHolder>
-        {yml.geekPal.map((item, i) => (
-          <React.Fragment key={i}>
-            {item.videoId === "" ? (
-              <StyledBackgroundSection
-                height="500px"
-                image={item.image.childImageSharp.gatsbyImageData}
-                bgSize="contain"
-                alt="geekforce image"
-              />
-            ) : (
-              <Div width="100%" height="500px" margin="0" overflow="hidden">
-                <ReactPlayer
-                  id={item.videoId}
-                  thumb={item.image}
-                  margin_tablet="0px"
-                  videoHeight="500px"
-                  videoHeight_tablet="450px"
-                  videoHeight_md="450px"
-                  bgSize="contain"
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                  }}
-                  margin="0"
-                />
-              </Div>
-            )}
-          </React.Fragment>
-        ))}
-      </VerticalVideoHolder>
-    </Div>
-  );
-};
-
-// Wrapper para componentes dinámicos con mejores props
-const DynamicComponentWrapper = ({
-  name,
-  componentData,
-  landingSections,
-  props,
-  session,
-  index,
-  yml,
-}) => {
-  const layout = componentData.layout || name;
-  const ComponentToRender = landingSections[layout];
-
-  if (!ComponentToRender) {
-    console.warn(`Component layout "${layout}" not found in landingSections`);
-    return null;
-  }
-
-  return ComponentToRender({
-    ...props,
-    yml: componentData,
-    session,
-    course: yml.meta_info?.utm_course,
-    location: componentData.meta_info?.utm_location,
-    index: index,
-  });
-};
-
-// Componente principal
 const GeekPal = (props) => {
   const { data, pageContext, yml } = props;
   const { session } = React.useContext(SessionContext);
+  const partnersData = data.allPartnerYaml.edges[0].node;
+  const content = data.allPageYaml.edges[0].node;
   const [components, setComponents] = React.useState({});
 
-  // Configuración de componentes dinámicos
+  const bulletIcons = [
+    {
+      icon: "elderly-fill",
+      background: "#0097CF",
+      transform: "rotate(180deg)",
+    },
+    {
+      icon: "square-bracket-fill",
+      background: "#000",
+    },
+    {
+      icon: "curly-bracket-fill",
+      background: "#FFB718",
+    },
+  ];
+
   useEffect(() => {
     let _components = {};
-    if (yml.components) {
+    if (yml.components)
       yml.components.forEach(({ name, ...rest }) => {
         _components[name] = rest;
       });
-    }
     setComponents({ ...yml, ..._components });
   }, [yml]);
 
-  // Componentes ordenados y filtrados
-  const orderedComponents = Object.keys(components)
-    .filter(
-      (name) =>
-        components[name] &&
-        (landingSections[name] || landingSections[components[name].layout])
-    )
-    .sort((a, b) => (components[b].position > components[a].position ? -1 : 1));
 
   return (
     <>
-      {/* Header Section con elementos gráficos distintivos */}
-      <GeekPalHeader yml={yml} session={session} />
+      {/* Header Section with decorative elements */}
+      <Grid
+        padding="24px 20px"
+        padding_tablet="100px 40px 40px 40px"
+        padding_md="100px 80px 40px 80px"
+        padding_lg="100px 0px 40px 0px"
+        columns_tablet="18"
+        margin="70px auto 24px auto"
+        maxWidth="1280px"
+        position="relative"
+        gridTemplateColumns_tablet="repeat(21, 1fr)"
+        gridGap="0px"
+      >
+        {/* Header decorative elements */}
+        <Img
+          src="/images/Ellipse-7.png"
+          width="450px"
+          height="300px"
+          backgroundSize="contain"
+          style={{
+            position: "absolute",
+            right: "-150px",
+            top: "-100px",
+          }}
+        />
+        <Img
+          src="/images/vector-stroke-light.png"
+          width="120px"
+          height="165px"
+          style={{
+            position: "absolute",
+            left: "48%",
+            top: "20%",
+          }}
+        />
 
-      {/* Dynamic Components con mejor estructura */}
-      <section aria-label="Dynamic Content Sections">
-        {orderedComponents.map((name, index) => (
-          <DynamicComponentWrapper
-            key={`${name}-${index}`}
-            name={name}
-            componentData={components[name]}
-            landingSections={landingSections}
-            props={props}
-            session={session}
-            index={index}
-            yml={yml}
+        <Div
+          flexDirection="column"
+          justifyContent_tablet="start"
+          gridColumn_tablet="1 / 11"
+          position="relative"
+        >
+          <Paragraph
+            fontSize="50px"
+            lineHeight="54px"
+            padding_xs="10px 0"
+            padding_md="0px"
+            margin="0"
+            textAlign_xs="center"
+            textAlign_tablet="left"
+            color="black"
+            fontSize_xs="40px"
+            lineHeight_xs="38px"
+            fontSize_tablet="50px"
+            lineHeight_tablet="54px"
+            dangerouslySetInnerHTML={{ __html: yml.header.title }}
           />
-        ))}
-      </section>
+          <Paragraph
+            fontSize="24px"
+            lineHeight="29px"
+            fontFamily="Lato-Bold"
+            padding_xs="10px 0px"
+            textAlign_xs="center"
+            textAlign_tablet="left"
+            padding_md="30px 0px 10px 0"
+            color="black"
+            dangerouslySetInnerHTML={{ __html: yml.header.paragraph }}
+          />
+
+          {/* Vector decorativo para el texto */}
+          <Img
+            src="/images/vector-stroke-left.png"
+            width="89px"
+            height="119px"
+            backgroundSize="contain"
+            style={{
+              position: "absolute",
+              left: "-62px",
+              bottom: "21px",
+              zIndex: "-1",
+            }}
+          />
+        </Div>
+
+        <Div
+          height="auto"
+          width="100%"
+          padding_xs="40px 0 0 0"
+          padding_tablet="15% 0 0 0"
+          padding_md="0% 16px 0 10%"
+          padding_lg="0 15px 0 10%"
+          gridColumn_tablet="12 / 22"
+          position="relative"
+        >
+          <Img
+            src="/images/Group-6400.png"
+            width="193px"
+            height="10px"
+            style={{
+              position: "absolute",
+            }}
+            left_xs="0"
+            bottom_tablet="10%"
+            left_md="-40%"
+            bottom_md="10%"
+            bottom_lg="0%"
+          />
+          <VerticalVideoHolder>
+            {yml.geekPal.map((item, i) => {
+              return (
+                <React.Fragment key={i}>
+                  {item.videoId === "" ? (
+                    <StyledBackgroundSection
+                      height="500px"
+                      image={item.image.childImageSharp.gatsbyImageData}
+                      bgSize="contain"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                      }}
+                      alt="geekforce image"
+                    />
+                  ) : (
+                    <Div
+                      width="100%"
+                      height="500px"
+                      margin="0"
+                      overflow="hidden"
+                    >
+                      <ReactPlayer
+                        id={item.videoId}
+                        thumb={item.image}
+                        margin_tablet="0px"
+                        videoHeight="500px"
+                        videoHeight_tablet="450px"
+                        videoHeight_md="450px"
+                        bgSize="contain"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                        }}
+                        margin="0"
+                      />
+                    </Div>
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </VerticalVideoHolder>
+        </Div>
+      </Grid>
+
+      {/* Dynamic Components */}
+      {Object.keys(components)
+        .filter(
+          (name) =>
+            components[name] &&
+            (landingSections[name] || landingSections[components[name].layout])
+        )
+        .sort((a, b) =>
+          components[b].position > components[a].position ? -1 : 1
+        )
+        .map((name, index) => {
+          const layout = components[name].layout || name;
+          return landingSections[layout]({
+            ...props,
+            yml: components[name],
+            session,
+            course: yml.meta_info?.utm_course,
+            location: components.meta_info?.utm_location,
+            index: index,
+          });
+        })}
     </>
   );
 };
@@ -296,9 +245,9 @@ export const query = graphql`
             image {
               childImageSharp {
                 gatsbyImageData(
-                  layout: CONSTRAINED
+                  layout: CONSTRAINED # --> CONSTRAINED || FIXED || FULL_WIDTH
                   width: 800
-                  placeholder: NONE
+                  placeholder: NONE # --> NONE || DOMINANT_COLOR || BLURRED | TRACED_SVG
                   quality: 100
                 )
               }
@@ -311,9 +260,9 @@ export const query = graphql`
             image {
               childImageSharp {
                 gatsbyImageData(
-                  layout: FULL_WIDTH
+                  layout: FULL_WIDTH # --> CONSTRAINED || FIXED || FULL_WIDTH
                   width: 1000
-                  placeholder: NONE
+                  placeholder: NONE # --> NONE || DOMINANT_COLOR || BLURRED | TRACED_SVG
                   quality: 100
                 )
               }
@@ -426,4 +375,5 @@ export const query = graphql`
     }
   }
 `;
+
 export default BaseRender(GeekPal);
