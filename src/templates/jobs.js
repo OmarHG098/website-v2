d8b5f634e649061d5eba79bbc84f6c9116049aac
OmarHG098 <omar_ehr@outlook.com>
2025-09-23 04:36:58 +0000

    ADD: new component to pages

--- src/templates/jobs.js
+++ src/templates/jobs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { Header } from "../components/Sections";
+import { GridContainer, Header, Div } from "../components/Sections";
 import BaseRender from "./_baseLayout";
 import JobInfo from "../components/JobInfo";
+import WorkTogether from "../components/WorkTogether";
 import { isCustomBarActive } from "../actions";
 import { SessionContext } from "../session";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const Jobs = ({ data, pageContext, yml }) => {
   const { session } = React.useContext(SessionContext);
@@ -12,13 +14,56 @@ const Jobs = ({ data, pageContext, yml }) => {
 
   return (
     <>
-      <Header
-        seo_title={yml.seo_title}
-        title={yml.header.title}
-        margin={isCustomBarActive(session) ? "120px 0 0 0" : "70px 0 0 0"}
-        paragraph={yml.header.paragraph}
-        padding_tablet="72px 0 40px 0"
-      />
+      <GridContainer
+        margin="20px auto 0 auto"
+        margin_tablet="auto"
+        padding_tablet="0"
+        padding_md="10rem 0 4rem"
+        containerColumns_tablet="1fr repeat(12,1fr) 1fr"
+        columns_tablet="2"
+        maxWidth="1280px"
+        gridColumn_tablet="1 / span 14"
+      >
+        <Header
+          hideArrowKey
+          textAlign_tablet="left"
+          seo_title={yml.seo_title}
+          title={yml.header.title}
+          paragraph={yml.header.paragraph}
+          fontSize_paragraph="21px"
+          padding_tablet="20px"
+          padding="0 10px"
+          margin="30px 0 0 0"
+          paddingTitle="0"
+          paddingParagraph="0"
+          position="relative"
+        />
+
+        <Div width="100%" height="100%">
+          <GatsbyImage
+            style={{
+              height: "390px",
+              minWidth: "150px",
+              width: "auto",
+              margin: "0 20px",
+            }}
+            imgStyle={{
+              objectFit: "contain",
+              WebkitUserDrag: "none",
+            }}
+            alt={yml.header.image_alt}
+            image={getImage(yml.header.image.childImageSharp.gatsbyImageData)}
+          />
+        </Div>
+      </GridContainer>
+      {yml.work_together && (
+        <WorkTogether
+          title={yml.work_together.title}
+          features={yml.work_together.features}
+          showImages={false}
+          showDescription={false}
+        />
+      )}
       <JobInfo lang={lang} />
     </>
   );
@@ -41,14 +86,25 @@ export const query = graphql`
           header {
             title
             paragraph
+            image_alt
             image {
               childImageSharp {
                 gatsbyImageData(
                   layout: CONSTRAINED # --> CONSTRAINED || FIXED || FULL_WIDTH
-                  width: 800
+                  width: 1200
+                  quality: 100
                   placeholder: NONE # --> NONE || DOMINANT_COLOR || BLURRED | TRACED_SVG
+                  breakpoints: [200, 340, 520, 890]
                 )
               }
+            }
+          }
+          work_together {
+            title
+            features {
+              title
+              icon
+              description
             }
           }
         }
