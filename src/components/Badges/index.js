729631e1f6c2dbb7a9fa83f80bf4ee3d15c6aff5
OmarHG098 <omar_ehr@outlook.com>
2025-10-28 22:03:50 +0000

    FIX: landing a render issues, UPDATE: landing a rule for imagge styling

--- src/components/Badges/index.js
+++ src/components/Badges/index.js
@@ -82,8 +82,7 @@ const SquaresVariant = ({
               initialSlide: 0,
               dots: true,
               arrows: true,
-              centerMode: true,
-              centerPadding: "40px",
+              centerMode: false,
             },
           },
         ],
@@ -400,7 +399,11 @@ const Badges = ({
       </CarouselV2>
 
       {short_link && (
-        <Div justifyContent="center" margin="60px 0 0 0" margin_tablet="20px 0 0 0">
+        <Div
+          justifyContent="center"
+          margin="60px 0 0 0"
+          margin_tablet="20px 0 0 0"
+        >
           <Link to={content.link_to}>
             <Paragraph width="auto" color={Colors.blue}>
               {`${content.short_link_text} >`}
